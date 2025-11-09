ab7ef6c88e26e1ca4cdff62202c082432f748af7
smolga2110 <73380083+smolga2110@users.noreply.github.com>
2025-11-09 22:03:01 +0300

    Initial commit: Add progress bar block and basic animation

---
+++ progress-bar.js
@@ -0,0 +1,70 @@
+class ProgressBar {
+
+    constructor(options = {}){
+        this.state = {
+            value: options.value || 0,
+            min: options.min || 0,
+            max: options.max || 100,
+            isAnimated: false,
+            isHidden: false
+        }
+    }
+
+    decrement(){
+        this.state.value--
+    }
+
+    increment(){
+        this.state.value++
+    }
+}
+
+let prog = new ProgressBar();
+
+function displayBar(){
+
+    const bar = document.querySelector('.progress-bar')
+    const elem = document.querySelector("progress")
+
+    bar.style.background = ''
+
+    elem.value = prog.state.value
+    bar.style.background = `
+      radial-gradient(closest-side, white 79%, transparent 80% 100%),
+      conic-gradient(hotpink ${prog.state.value}%, pink 0)
+    `;
+
+}
+
+function animationDisplay(){
+    const bar = document.querySelector('.progress-bar')
+    if (!prog.state.isAnimated){
+        bar.classList.remove('animate');
+        bar.style.background = ''
+
+        bar.classList.add('animate');
+        prog.state.isAnimated = true;
+    }
+    else{
+        bar.classList.remove('animate');
+        displayBar()
+        prog.state.isAnimated = false;
+    }
+}
+
+function incrementValue(){
+    prog.increment()
+    displayBar()
+}
+
+function decrementValue(){
+    prog.decrement()
+    displayBar()
+}
+
+function changeValue(value){
+    prog.state.value = value
+    displayBar()
+}
+
+window.onload = displayBar;
